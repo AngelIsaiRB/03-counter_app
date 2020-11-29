bef8b6453245c422b3460652627419b666a0dd96
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-11-28 19:40:14 -0600

    impresion components

--- counter-app/src/PrimeraApp.js
+++ counter-app/src/PrimeraApp.js
@@ -1,10 +1,24 @@
 import React from 'react';
+// import React, { Fragment } from 'react';
 
 // functional components(SFC)
 
 const PrimeraApp = ()=>{
-
-    return <h1>Hola mundo</h1>;
+    const saludo="hola mundo const";
+    const boleano = true;
+    const areglo = [1,2,3,4,5,6];
+    const objeto = {
+        nombre:"isai",
+        edad : 23,
+    }
+    return (
+        <>
+            <h1>{saludo, boleano} </h1>
+            <h2>{areglo}</h2>
+            <pre>{ JSON.stringify(objeto,null,3)}</pre>
+            <p>Mi primera app</p>
+        </>
+    );
 
 }
 
